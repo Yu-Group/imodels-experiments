ae6e7b632cae8de298d0dd47f6ca7a6b27331407
OmerRonen <omerronen10@gmail.com>
2022-01-12 13:02:50 -0800

    gosdt

--- viz_godst.sh
+++ viz_godst.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-JOB_NAME=$0
-#SBATCH --job-name=$JOB_NAME
+
+#SBATCH --job-name=gosdt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=omer_ronen@berkeley.edu
-#SBATCH -o $JOB_NAME.out #File to which standard out will be written
-#SBATCH -e $JOB_NAME.err #File to which standard err will be written
-python -m viz
+#SBATCH -o gosdt.out #File to which standard out will be written
+#SBATCH -e gosdt.err #File to which standard err will be written
+python -m 01_fit_models --config shrinkage --classification --split_seed $0
