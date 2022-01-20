b8fd2d0af50f99d84094b895912a9608e0597508
OmerRonen <omerronen10@gmail.com>
2022-01-19 19:30:55 -0800

    time gosdt config

--- viz_godst.sh
+++ viz_godst.sh
@@ -3,6 +3,5 @@
 #SBATCH --job-name=gosdt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=omer_ronen@berkeley.edu
-#SBATCH -o gosdt.out #File to which standard out will be written
-#SBATCH -e gosdt.err #File to which standard err will be written
+
 /accounts/campus/omer_ronen/.conda/envs/tree_shrink/bin/python -m 01_fit_models --config shrinkage --classification --split_seed $1 --reg $2
