f39b5aa6a46ec1ce147a600ef9f8e2363f6ae801
OmerRonen <omerronen10@gmail.com>
2022-01-26 15:00:27 -0800

    more mem

--- run_gosdt_slurm.py
+++ run_gosdt_slurm.py
@@ -6,7 +6,7 @@ import numpy as np
 def main():
     for seed in range(9):
         for reg in np.arange(0, 0.0051, 0.001):
-            cmd = f"sbatch -p jsteinhardt -C bigmem viz_godst.sh {seed} {reg}"
+            cmd = f"sbatch -p jsteinhardt -C xlmem viz_godst.sh {seed} {reg}"
             subprocess.call(cmd, shell=True)
 
 
