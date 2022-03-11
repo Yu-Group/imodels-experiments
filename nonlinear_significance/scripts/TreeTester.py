fb1835581cdeb72dedfac2bc281eea40e9525c20
aagarwal1996 <aa3797@berkeley.edu>
2022-03-10 16:20:47 -0800

    fixing testing code

--- nonlinear_significance/scripts/TreeTester.py
+++ nonlinear_significance/scripts/TreeTester.py
@@ -49,8 +49,11 @@ class TreeTester(TransformerMixin, BaseEstimator):
                     #P_j[feat,feat] = 1
                         P_j[row_num,feat] = 1.0
                     p_vals[i,j] = OLS_results.wald_test(P_j,scalar = False).pvalue
-        p_vals[np.isnan(p_vals)] = 1.0
-        return np.median(p_vals,axis=0)
+        median_p_vals = 2*np.median(p_vals,axis=0)
+        median_p_vals[median_p_vals > 1.0] = 1.0
+        return self.multiple_testing_correction(median_p_vals)
+
+
     
     def multiple_testing_correction(self,p_vals,method = 'bonferroni',alpha = 0.05):
         return smt.multipletests(p_vals, method=method)[1] 
@@ -71,27 +74,27 @@ class optimalTreeTester(TransformerMixin, BaseEstimator): #This class is trying 
             tree_transformer_sel = TreeTransformer(estimator = self.estimator, max_components=self.max_components) 
             tree_transformer_sel.fit(X_sel) 
             transformed_feats_sel = tree_transformer_sel.transform(X_sel)
-#            print("sel size is: " + str(transformed_feats_sel.shape))
 
             tree_transformer_inf = TreeTransformer(estimator = self.estimator, max_components=self.max_components) 
             tree_transformer_inf.fit(X_inf) 
             transformed_feats_inf = tree_transformer_inf.transform(X_inf) 
-            #print("inf size is: " + str(transformed_feats_inf.shape))
             
-            #print("done transforming")
             sigma_sel = (np.sum((y_sel - np.mean(y_sel))**2))/(len(y_sel)-1)
             sigma_inf = (np.sum((y_inf - np.mean(y_inf))**2))/(len(y_inf)-1)
 
             if eta is None:
                 eta = sigma_sel
             for j in range(X.shape[1]):
-                stumps_sel_for_feat = tree_transformer_sel.original_feat_to_transformed_mapping[j]
-                stumps_inf_for_feat = tree_transformer_inf.original_feat_to_transformed_mapping[j]
-                X_sel_for_feat = transformed_feats_sel[:,stumps_sel_for_feat]
-                X_inf_for_feat = transformed_feats_inf[:,stumps_inf_for_feat]
-                #print(X_for_feat.shape)
-                optimal_lambda_for_feat = self.get_optimal_lambda(X_sel_for_feat,y_sel,eta,sigma_sel,lr,n_steps)
-                p_vals[i,j] = self.compute_p_val(optimal_lambda_for_feat,X_inf_for_feat,y_inf,sigma_inf,num_reps)
+                stumps_sel_for_feat = tree_transformer_sel.original_feat_to_stump_mapping[j]
+                num_splits_for_feat = len(stumps_sel_for_feat)
+                if(num_splits_for_feat) == 0:
+                    p_vals[i,j] = 1.0
+                else:
+                    stumps_inf_for_feat = tree_transformer_inf.original_feat_to_stump_mapping[j]
+                    X_sel_for_feat = transformed_feats_sel[:,stumps_sel_for_feat]
+                    X_inf_for_feat = transformed_feats_inf[:,stumps_inf_for_feat]
+                    optimal_lambda_for_feat = self.get_optimal_lambda(X_sel_for_feat,y_sel,eta,sigma_sel,lr,n_steps)
+                    p_vals[i,j] = self.compute_p_val(optimal_lambda_for_feat,X_inf_for_feat,y_inf,sigma_inf,num_reps)
             
         median_p_vals = 2*np.median(p_vals,axis=0)
         median_p_vals[median_p_vals > 1.0] = 1.0
@@ -100,9 +103,7 @@ class optimalTreeTester(TransformerMixin, BaseEstimator): #This class is trying 
     
     def multiple_testing_correction(self,p_vals,method = 'bonferroni',alpha = 0.05):
         return smt.multipletests(p_vals, method=method)[1] 
-
-           
-    
+             
     def compute_p_val(self,optimal_lambda,X_inf,y_inf,sigma_inf,num_reps):
         u_inf, s_inf, vh_inf = np.linalg.svd(X_inf, full_matrices=False)
         optimal_weights = optimal_lambda.cpu().detach().numpy()
@@ -114,7 +115,7 @@ class optimalTreeTester(TransformerMixin, BaseEstimator): #This class is trying 
         
     def get_optimal_lambda(self,X,y,eta,sigma_sel,lr,n_steps):
         u_sel, s_sel, vh_sel = np.linalg.svd(X, full_matrices=False)
-        tns = torch.distributions.Uniform(0, 0.1).sample((u_sel.shape[1],))
+        tns = torch.distributions.Uniform(0, 1.0).sample((u_sel.shape[1],))
         weights = Variable(tns, requires_grad=True)
         opt = torch.optim.SGD([weights], lr=lr)
         for i in range(n_steps):
@@ -128,8 +129,8 @@ class optimalTreeTester(TransformerMixin, BaseEstimator): #This class is trying 
         T1 = torch.from_numpy(np.transpose(u_sel) @ y_sel)
         T1 = T1.type(torch.FloatTensor)
         T1 = weights * T1
-        T1 = torch.linalg.vector_norm(T1)
-        T2 = eta*torch.sum(weights**4)
+        T1 = torch.linalg.vector_norm(T1)**2
+        T2 = eta*torch.sum(weights**2)
         T3 = sigma_sel*torch.sqrt(torch.sum(weights**4))
         return torch.divide(torch.subtract(T2,T1),T3)
     
