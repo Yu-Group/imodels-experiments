03a5967375e553deab285276cfd0fb55f35b07eb
Yan Shuo Tan <yanshuo@gmail.com>
2022-04-30 17:25:02 -0700

    Fixed a bug when number of stumps on a given feature is 0

--- nonlinear_significance/scripts/util.py
+++ nonlinear_significance/scripts/util.py
@@ -193,7 +193,9 @@ class TreeTransformer(TransformerMixin, BaseEstimator):
                 max_components = int(self.max_components * X.shape[0])
             restricted_stumps = [self.all_stumps[idx] for idx in self.original_feat_to_stump_mapping[k]]
             n_stumps = len(restricted_stumps)
-            if always_pca or (n_stumps > max_components):  # self.max_components:
+            if n_stumps == 0:
+                pca = None
+            elif always_pca or (n_stumps > max_components):  # self.max_components:
                 transformed_feature_vectors = tree_feature_transform(restricted_stumps, X)
                 pca = PCA(n_components=max_components)  # self.max_components)
                 pca.fit(transformed_feature_vectors)
