2c43a924b48aa7ecf4699b922625d7357f775d0a
Abhineet Agarwal <aa3797@ln002.brc>
2022-09-13 12:15:49 -0700

    changing regression sims config folder structure

---
+++ feature_importance/fi_config/gmdi/regressions_sims/pecarntbi/linear_dgp/dgp.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("../..")
+from feature_importance.scripts.simulations_util import *
+
+X_DGP = sample_real_X
+X_PARAMS_DICT = {
+    "fpath": "data/X_tbipecarn.csv",
+    "sample_row_n": None,
+    "sample_col_n": None
+}
+Y_DGP = linear_model
+Y_PARAMS_DICT = {
+    "beta": 1,
+    "sigma": None,
+    "heritability": 0.4,
+    "s":5,
+}
+
+VARY_PARAM_NAME = ["heritability", "sample_row_n"]
+VARY_PARAM_VALS = {"heritability": {"0.1": 0.1, "0.2": 0.2, "0.4": 0.4, "0.8": 0.8},
+                   "sample_row_n": {"100": 100, "250": 250, "500": 500,"1000":1000,"1500":1500}}
