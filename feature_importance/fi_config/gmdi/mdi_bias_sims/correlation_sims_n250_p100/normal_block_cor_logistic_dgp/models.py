367bef893ab67e4dfd83368cf0a75cce7143552d
Abhineet Agarwal <aa3797@ln003.brc>
2022-10-10 15:07:04 -0700

    modifying config files

--- feature_importance/fi_config/gmdi/mdi_bias_sims/correlation_sims_n250_p100/normal_block_cor_logistic_dgp/models.py
+++ feature_importance/fi_config/gmdi/mdi_bias_sims/correlation_sims_n250_p100/normal_block_cor_logistic_dgp/models.py
@@ -1,26 +1,25 @@
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from feature_importance.util import ModelConfig, FIModelConfig
-from feature_importance.scripts.competing_methods import tree_mdi, tree_mdi_OOB, tree_perm_importance, tree_shap
+from feature_importance.scripts.competing_methods import tree_mda,tree_mdi, tree_mdi_OOB, tree_perm_importance, tree_shap
 from sklearn.metrics import r2_score, log_loss
 from imodels.importance.r2f_exp_cleaned import GMDI_pipeline, RidgeLOOPPM
 
 
 ESTIMATORS = [
     [ModelConfig('RF', RandomForestClassifier, model_type='tree',
-                 other_params={'n_estimators': 100, 'min_samples_leaf': 1, 'max_features': 'sqrt', 'random_state': 42})],
-    [ModelConfig('RF', RandomForestRegressor, model_type='tree_reg',
-                 other_params={'n_estimators': 100, 'min_samples_leaf': 1, 'max_features': 'sqrt', 'random_state': 42})]
+                 other_params={'n_estimators': 100, 'min_samples_leaf': 1, 'max_features': 'sqrt', 'random_state': 21})],
 ]
 
 FI_ESTIMATORS = [
-    [FIModelConfig('GMDI_logistic_auroc', GMDI_pipeline, model_type='tree', other_params = {'regression': False})],
-    [FIModelConfig('GMDI_logistic_logloss', GMDI_pipeline, model_type='tree', ascending=False, other_params = {'regression': False, 'scoring_fn': log_loss})],
-    [FIModelConfig('GMDI_ridge', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'partial_prediction_model': RidgeLOOPPM(), 'scoring_fn': r2_score})],
-    [FIModelConfig('GPermutation_logistic_auroc', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'mode': 'keep_rest'})],
-    [FIModelConfig('GPermutation_logistic_logloss', GMDI_pipeline, model_type='tree', ascending=False, other_params = {'regression': False, 'mode': 'keep_rest', 'scoring_fn': log_loss})],
-    [FIModelConfig('GPermutation_ridge', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'mode': 'keep_rest', 'partial_prediction_model': RidgeLOOPPM(), 'scoring_fn': r2_score})],
+    #[FIModelConfig('GMDI_logistic_auroc', GMDI_pipeline, model_type='tree', other_params = {'regression': False})],
+    [FIModelConfig('GMDI_logistic_logloss_always_include_raw', GMDI_pipeline, model_type='tree', ascending=False, other_params = {'regression': False, 'scoring_fn': log_loss,"drop_features":False})],
+    [FIModelConfig('GMDI_ridge_always_include_raw', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'partial_prediction_model': RidgeLOOPPM(), 'scoring_fn': r2_score,"drop_features": False})],
+    #[FIModelConfig('GPermutation_logistic_auroc', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'mode': 'keep_rest'})],
+    #[FIModelConfig('GPermutation_logistic_logloss', GMDI_pipeline, model_type='tree', ascending=False, other_params = {'regression': False, 'mode': 'keep_rest', 'scoring_fn': log_loss})],
+    #[FIModelConfig('GPermutation_ridge', GMDI_pipeline, model_type='tree', other_params = {'regression': False, 'mode': 'keep_rest', 'partial_prediction_model': RidgeLOOPPM(), 'scoring_fn': r2_score})],
     [FIModelConfig('MDI', tree_mdi, model_type='tree')],
     [FIModelConfig('MDI-oob', tree_mdi_OOB, model_type='tree_reg')],
     [FIModelConfig('Permutation', tree_perm_importance, model_type='tree')],
-    [FIModelConfig('TreeSHAP', tree_shap, model_type='tree_reg')]
+    [FIModelConfig('TreeSHAP', tree_shap, model_type='tree_reg')],
+    [FIModelConfig('MDA',tree_mda,model_type = 'tree')]
 ]
--- feature_importance/fi_config/gmdi/regressions_sims/compas_hier_poly_3m_2r_dgp/models.py
+++ feature_importance/fi_config/gmdi/regressions_sims/compas_hier_poly_3m_2r_dgp/models.py
@@ -1,23 +1,27 @@
 from sklearn.ensemble import RandomForestRegressor
 from feature_importance.util import ModelConfig, FIModelConfig
-from feature_importance.scripts.competing_methods import lin_reg_marginal_t_test,tree_mdi, tree_mdi_OOB, tree_perm_importance, tree_shap
+from feature_importance.scripts.competing_methods import tree_mda,lin_reg_marginal_t_test,tree_mdi, tree_mdi_OOB, tree_perm_importance, tree_shap,gjMDI
 from imodels.importance.r2f_exp_cleaned import GMDI_pipeline
 
 
 ESTIMATORS = [
     [ModelConfig('RF', RandomForestRegressor, model_type='tree',
-                 other_params={'n_estimators': 100, 'min_samples_leaf': 5, 'max_features': 0.33, 'random_state': 42})]
+                 other_params={'n_estimators': 100, 'min_samples_leaf': 5, 'max_features': 0.33, 'random_state': 27})]
 ]
 
 FI_ESTIMATORS = [
     [FIModelConfig('GMDI_ridge', GMDI_pipeline, model_type='tree')],
-    [FIModelConfig('GMDI_ridge_oob', GMDI_pipeline, model_type='tree', other_params = {'subsetting_scheme': 'oob'})],
-    [FIModelConfig('GPermutation_ridge', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest'})],
-    [FIModelConfig('GPermutation_ridge_oob', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest', 'subsetting_scheme': 'oob'})],
+    #[FIModelConfig('GMDI_ridge_oob', GMDI_pipeline, model_type='tree', other_params = {'subsetting_scheme' : 'oob'})],
+    #[FIModelConfig('GMDI_ridge_always_include_raw', GMDI_pipeline, model_type = 'tree', other_params = {'drop_features' : False})],
+    #[FIModelConfig('GMDI_ridge_always_include_raw_oob', GMDI_pipeline, model_type = 'tree', other_params = {'drop_features' : False, 'subsetting_scheme' : 'oob'})],
+    #[FIModelConfig('GPermutation_ridge', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest'})],
+    #[FIModelConfig('GPermutation_ridge_oob', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest', 'subsetting_scheme': 'oob'})],
+    #[FIModelConfig('GPermutation_ridge_always_include_raw', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest','drop_features':False})],
+    #[FIModelConfig('GPermutation_ridge_oob_always_include_raw', GMDI_pipeline, model_type='tree', other_params = {'mode': 'keep_rest', 'subsetting_scheme': 'oob','drop_features':False})],
     [FIModelConfig('MDI', tree_mdi, model_type='tree')],
     [FIModelConfig('MDI-oob', tree_mdi_OOB, model_type='tree')],
     [FIModelConfig('Permutation', tree_perm_importance, model_type='tree')],
     [FIModelConfig('TreeSHAP', tree_shap, model_type='tree')],
-    [FIModelConfig('Ttest',lin_reg_marginal_t_test,ascending = False,model_type = 'linear')],
+    #[FIModelConfig('Ttest',lin_reg_marginal_t_test,ascending = False,model_type = 'linear')],
     [FIModelConfig('MDA',tree_mda,model_type = 'tree')],                                                                                      
 ]
