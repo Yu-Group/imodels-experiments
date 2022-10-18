3c5d9bc963a4324751fad0c4a89c3d6d4fd5d905
Tiffany Meihui Tang <tiffanytang@n0003.testbed0>
2022-10-18 10:36:25 -0700

    add correlation metric

--- feature_importance/scripts/metrics.py
+++ feature_importance/scripts/metrics.py
@@ -1,4 +1,11 @@
 import numpy as np
+from scipy.stats import pearsonr
+
+
+def corr(y_true, y_pred):
+  if np.all(y_true == y_true[0]) or np.all(y_pred == y_pred[0]):
+    return 0
+  return pearsonr(y_true, y_pred)[0]
 
 
 def metric_multiclass_wrapper(metric_fun):
