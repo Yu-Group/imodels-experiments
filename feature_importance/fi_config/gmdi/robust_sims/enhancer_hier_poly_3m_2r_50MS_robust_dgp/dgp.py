3163c0c30d5f0a1028eb614fdc0f0c9a3bdd14b0
Abhineet Agarwal <aa3797@ln001.brc>
2022-11-01 15:14:45 -0700

    adding robust DGP configs

---
+++ feature_importance/fi_config/gmdi/robust_sims/enhancer_hier_poly_3m_2r_50MS_robust_dgp/dgp.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("../..")
+from feature_importance.scripts.simulations_util import *
+
+X_DGP = sample_real_X
+X_PARAMS_DICT = {
+    "fpath": "data/cleaned_data/X_enhancer_uncorrelated_log_transformed.csv",
+    "sample_row_n": None,
+    "sample_col_n": None
+}
+Y_DGP = hierarchical_poly
+Y_PARAMS_DICT = {
+    "beta": 1,
+    "sigma": None,
+    "heritability": 0.4,
+    "m": 3,
+    "r": 2,
+    "corrupt_how": "leverage_normal",
+    "corrupt_size": 0.1,
+    "corrupt_mean": 50  # 10, 25, 50, 100
+}
+
+VARY_PARAM_NAME = ["corrupt_size", "sample_row_n"]
+VARY_PARAM_VALS = {"corrupt_size": {"0": 0, "0.01": 0.005, "0.025": 0.0125, "0.05": 0.025, "0.1": 0.05},
+                   "sample_row_n": {"100": 100, "250": 250, "500": 500, "1000": 1000, "1500": 1500}}
---
+++ feature_importance/fi_config/gmdi/robust_sims/enhancer_hier_poly_3m_2r_50MS_robust_dgp/models.py
@@ -0,0 +1,21 @@
+from sklearn.ensemble import RandomForestRegressor
+from feature_importance.util import ModelConfig, FIModelConfig
+from feature_importance.scripts.competing_methods import tree_mda,lin_reg_marginal_t_test,tree_mdi, tree_mdi_OOB, tree_perm_importance, tree_shap,gjMDI
+from imodels.importance.r2f_exp_cleaned import GMDI_pipeline, RidgePPM, RobustLOOPPM, huber_loss
+from feature_importance.scripts.metrics import corr
+import numpy as np
+ESTIMATORS = [
+    [ModelConfig('RF', RandomForestRegressor, model_type='tree',
+                 other_params={'n_estimators': 100, 'min_samples_leaf': 5, 'max_features': 0.33, 'random_state': 27})]
+]
+
+
+FI_ESTIMATORS = [
+    [FIModelConfig('GMDI_ridge_loo_r2', GMDI_pipeline, model_type='tree')],
+    [FIModelConfig('GMDI_Huber_loo_huber_loss',GMDI_pipeline,model_type = 'tree',other_params = {'partial_prediction_model': RobustLOOPPM(max_iter = 2000), 'scoring_fn': huber_loss})],
+    #[FIModelConfig('MDI', tree_mdi, model_type='tree')],
+    [FIModelConfig('MDI-oob', tree_mdi_OOB, model_type='tree')],
+    #[FIModelConfig('Permutation', tree_perm_importance, model_type='tree')],
+    #[FIModelConfig('MDA', tree_mda, model_type='tree')],
+    #[FIModelConfig('TreeSHAP', tree_shap, model_type='tree')]
+]
