6e1ac104e920edabbc2940a50e3dda1a96e9feca
Tiffany Meihui Tang <tiffanytang@n0025.savio3>
2023-01-12 22:12:51 -0800

    update gmdi fi_config

--- feature_importance/fi_config/gmdi/other_regression_sims/modeling_choices_min_samples5/enhancer_hier_poly_3m_2r_dgp/models.py
+++ feature_importance/fi_config/gmdi/other_regression_sims/modeling_choices_min_samples5/enhancer_hier_poly_3m_2r_dgp/models.py
@@ -10,14 +10,9 @@ ESTIMATORS = [
 
 FI_ESTIMATORS = [
     [FIModelConfig('MDI', tree_mdi, model_type='tree')],
-    [FIModelConfig('GMDI_basic', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'inbag', 'include_raw': False, 'partial_prediction_model': RidgePPM(loo=False, alpha_grid=1e-6)})],
-    [FIModelConfig('GMDI_loo', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'loo', 'include_raw': False, 'partial_prediction_model': RidgePPM(loo=False, alpha_grid=1e-6)})],
-    [FIModelConfig('GMDI_raw', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'inbag', 'include_raw': True, 'partial_prediction_model': RidgePPM(loo=False, alpha_grid=1e-6)})],
-    [FIModelConfig('GMDI_ridge', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'inbag', 'include_raw': False, 'partial_prediction_model': RidgePPM(loo=False)})],
-    [FIModelConfig('GMDI_ridge_raw', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'inbag'})],
-    [FIModelConfig('GMDI_ridge_loo', GMDI_pipeline, model_type='tree', other_params={'include_raw': False})],
-    [FIModelConfig('GMDI_ridge_raw_oob', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'oob', 'partial_prediction_model': RidgePPM(loo=False)})],
-    [FIModelConfig('GMDI_ridge_raw_loo', GMDI_pipeline, model_type='tree')],
-    [FIModelConfig('GMDI_ols_raw_loo', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'loo', 'include_raw': True, 'partial_prediction_model': RidgePPM(loo=True, alpha_grid=1e-6)})],
+    [FIModelConfig('GMDI_loo', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'loo', 'include_raw': False, 'partial_prediction_model': RidgePPM(loo=False, alpha_grid=1e-6, gcv_mode='eigen')})],
+    [FIModelConfig('GMDI_raw', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'inbag', 'include_raw': True, 'partial_prediction_model': RidgePPM(loo=False, alpha_grid=1e-6, gcv_mode='eigen')})]
+    [FIModelConfig('GMDI_ridge_raw_loo', GMDI_pipeline, model_type='tree', other_params={'partial_prediction_model': RidgePPM(gcv_mode='eigen')})],
+    [FIModelConfig('GMDI_ols_raw_loo', GMDI_pipeline, model_type='tree', other_params={'sample_split': 'loo', 'include_raw': True, 'partial_prediction_model': RidgePPM(loo=True, alpha_grid=1e-6, gcv_mode='eigen')})],
     [FIModelConfig('MDI-oob', tree_mdi_OOB, model_type='tree')]
 ]
