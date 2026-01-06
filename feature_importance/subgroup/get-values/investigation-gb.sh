e09812f7a43fefc52330622b95e6080a67006ef8
zachrewolinski <zrewolinski@gmail.com>
2026-01-05 20:56:39 -0800

    updated subgroup experiments

--- feature_importance/subgroup/get-values/investigation-gb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=yugroup
-#SBATCH --cpus-per-task=4
-
-source activate mdi
-command="investigation_gb.py --dataname ${1} --seed ${2} --method ${3}"
-
-# Execute the command
-python $command
